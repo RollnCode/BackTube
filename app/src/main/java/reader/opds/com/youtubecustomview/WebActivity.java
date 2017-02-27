eee999ffb35bc01f81c28e5df04a9e2e52fd6ec8
Dmytriy Ogirenko <rangerover12@gmail.com>
2017-02-27 09:45:41 +0200

    1. refactor web activity to proper state;

--- app/src/main/java/reader/opds/com/youtubecustomview/WebActivity.java
+++ app/src/main/java/reader/opds/com/youtubecustomview/WebActivity.java
@@ -8,16 +8,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity implements ValueCallback<String> {
 
+    /**
+     * This is constants for tags and class names from youtube video html page, which must be clicked
+     */
+    private final String mTagName = "\"iframe\"";
+    private final String mClassName = "\"html5-video-player ytp-new-infobar ytp-hide-controls ytp-small-mode ytp-native-controls playing-mode ytp-touch-mode\"";
+
     private static final String TAG = WebActivity.class.getName();
-    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +36,18 @@ public class WebActivity extends AppCompatActivity implements ValueCallback<Stri
 
     @SuppressLint({"NewApi", "SetJavaScriptEnabled"})
     private void setWebView() {
-        mWebView = (WebView) findViewById(R.id.webView);
-        WebSettings settings = mWebView.getSettings();
+        WebView webView = (WebView) findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
 
         settings.setJavaScriptEnabled(true);
         settings.setBuiltInZoomControls(true);
         settings.setPluginState(WebSettings.PluginState.ON);
-        mWebView.setKeepScreenOn(true);
-        mWebView.setHorizontalScrollBarEnabled(false);
-        mWebView.setVerticalScrollBarEnabled(false);
+        webView.setKeepScreenOn(true);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setVerticalScrollBarEnabled(false);
 
 //        mWebView.setWebChromeClient(new WebChromeClient());
-        mWebView.setWebViewClient(new WebViewClient() {
+        webView.setWebViewClient(new WebViewClient() {
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -55,16 +58,23 @@ public class WebActivity extends AppCompatActivity implements ValueCallback<Stri
             public void onPageFinished(WebView view, String url) {
                 Log.d(TAG, "WebViewClient onPageFinished url: " + url);
 
-                mWebView.evaluateJavascript("document.getElementsByTagName(\"iframe\")[0].contentWindow" +
-                        ".document.getElementsByClassName(\"html5-video-player ytp-new-infobar ytp-hide-controls ytp-small-mode ytp-native-controls playing-mode ytp-touch-mode\")" +
-                        "[0].click();", WebActivity.this);
+                String evaluateString = String.format("document.getElementsByTagName(%s)[0]" +
+                                ".contentWindow.document.getElementsByClassName(%s)[0].click();",
+                        mTagName, mClassName);
+
+                view.evaluateJavascript(evaluateString, WebActivity.this);
             }
         });
 
+        final String videoId = "d9-zYbhDbPo";
+        final String iframeId = "iframe1";
+        final String autoPlay = "0";
+        final String enableJsApi = "1";
         final String mimeType = "text/html";
         final String encoding = "UTF-8";
-        String html = getHTML();
-        mWebView.loadDataWithBaseURL("", html, mimeType, encoding, "");
+
+        String html = getHTML(iframeId ,videoId, autoPlay, enableJsApi);
+        webView.loadDataWithBaseURL("", html, mimeType, encoding, "");
     }
 
     @Override
@@ -72,9 +82,10 @@ public class WebActivity extends AppCompatActivity implements ValueCallback<Stri
         Log.d(TAG, "Evaluate javascript: " + value);
     }
 
-    private String getHTML() {
-        return "<iframe id=iframe1\" crossorigin=\"anonymous\" width=\"420\" height=\"315\" src=\"http://www.youtube.com/embed/d9-zYbhDbPo" +
-                "?rel=0&autoplay=0&enablejsapi=1 frameborder=\"0\" allowfullscreen></iframe>";
+    private String getHTML(String iframeId, String videoId, String autoPlay, String enableJsApi) {
+        return String.format("<iframe id=%s crossorigin=\"anonymous\" width=\"420\" height=\"315\" " +
+                "src=\"http://www.youtube.com/embed/%s?rel=0&autoplay=%s&enablejsapi=%s frameborder=\"0\" allowfullscreen></iframe>",
+                iframeId, videoId, autoPlay, enableJsApi) ;
     }
 
 }
